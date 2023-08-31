ed076bf64d530bcc1887815ac929b6f3a89dbd58
crowsCaledfwlch <122137319+crowsCaledfwlch@users.noreply.github.com>
2023-08-31 17:54:54 -0400

    GUI Update
    Changed the GUI to have checkboxes and to have a basis for how the program reacts if it doesn't have enough data

--- GUI.py
+++ GUI.py
@@ -1,9 +1,13 @@
 import tkinter as tk
 import tkinter.font as tkFont
+from typing import Type
 
 # globals
-labels = ["gender", "age", "education", "currentSmoker", "cigsPerDay", "BPMeds", "prevalentStroke", "prevalentHyp", "totChol", "sysBP", "diaBP", "BMI", "heartRate", "glucose", "TenYearCHD", "Prediction"]
-entries = []          
+labels = ["Male", "Age", "Education*", "Current Smoker", "Cigs Per Day", "Takes Blood Pressure Meds", "Prevalent Stroke", "Prevalent Hypertensive", "Total Cholesterol", "Systolic Blood Pressure", "Diastolic Blood Pressure", "BMI", "Heart Rate", "Glucose", "TenYearCHD**", "Prediction"]
+entries = []
+dataWeight = [.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1] # will get the weights from the ML
+
+MinReq = .7 # will probably stay about the same
 
 # simplifies the creation of a frame
 def basicFrame(window):
@@ -11,7 +15,19 @@ def basicFrame(window):
 
 # Predict button's function
 def predictClick(lbl):
-    lbl.config(text="No ML Yet!!")
+    PredictionData = []
+    ProbabiltyOfData = 0
+    for i in range(len(entries)):
+       PredictionData.append(entries[i].get())
+       print(entries[i].get())
+       if PredictionData[i] != "":
+            ProbabiltyOfData += dataWeight[i]
+    if ProbabiltyOfData > MinReq:
+        lbl.config(text="No ML Yet!!")
+        return PredictionData # will instead call for a prediction using the prediction data
+    else:
+        lbl.config(text="Not enough data!")
+        return
 
 # exit button's code
 def exitClick():
@@ -20,12 +36,12 @@ def exitClick():
 def genWindow():
     # Creates new window
     window = tk.Tk()
-    fontObj = tkFont.Font(size=16)
+    fontObj = tkFont.Font(size=12)
     predictionLabel = tk.Label()
     # Sets column and row settings
     for i in range(9):
         window.columnconfigure(i, weight=1, minsize=75)
-    for i in range(6):
+    for i in range(7):
         window.rowconfigure(i, weight=1, minsize=60)
     # Creates the labels and entries for the data
     for i in range(len(labels)):
@@ -41,9 +57,15 @@ def genWindow():
             entFrame.grid(row=4, column=i//2, padx=2, pady=5)
         lbl = tk.Label(master=lblFrame, text=labels[i], font=fontObj)
         if(i < len(labels)-1):
-            ent = tk.Entry(master=entFrame, font=fontObj)
-            ent.pack()
-            entries.append(ent)
+            if(i == 0 or i == 3 or i == 5 or i == 6 or i == 7):
+                Variable1 = tk.IntVar()
+                ent = tk.Checkbutton(master=entFrame, variable=Variable1)    
+                entries.append(Variable1)
+                ent.pack()
+            else:
+                ent = tk.Entry(master=entFrame, font=fontObj)    
+                entries.append(ent)
+                ent.pack()
         else:
             predictionLabel = tk.Label(master=entFrame, text="No Prediction Yet", font=fontObj)
             predictionLabel.pack()
@@ -55,15 +77,20 @@ def genWindow():
     newLabel = tk.Label(master=frame1, text="Dx Assistant", font=fontObj)
     newLabel.pack()
     
+    addendumFrame = basicFrame(window)
+    addendumFrame.grid(row=5, column=0, columnspan=8, padx=5, pady=5)
+    addendumLabel = tk.Label(master=addendumFrame, text="*Some high school (1), high school/GED (2), some college/vocational school (3), college (4)\n**The 10 year risk of coronary heart disease(CHD).")
+    addendumLabel.pack()
+
     # Creates the exit button
     frame2 = basicFrame(window)
-    frame2.grid(row=5, column=8, padx=5, pady=5)
+    frame2.grid(row=6, column=8, padx=5, pady=5)
     exitButton = tk.Button(master=frame2, text="EXIT", font=fontObj, command=exitClick)
     exitButton.pack()
     
     # Creates the "predict" button
     frame3 = basicFrame(window)
-    frame3.grid(row=5, column=3,columnspan=2, padx=5, pady=5)
+    frame3.grid(row=6, column=3,columnspan=2, padx=5, pady=5)
     predictButton = tk.Button(master=frame3, text="PREDICT", font=fontObj, command=lambda: predictClick(predictionLabel))
     predictButton.pack()
 
